f4086a7d60d22c58171ab008825305f82de1baf5
Jakob Malmgren <Jakob.Malmgren1987@gmail.com>
2025-05-29 17:02:11 +0200

    la in en ikon o ändra align

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,83 +45,54 @@ const skills = [
 const resumeSkill = [
   {
     image: "/images/atom.png",
-    skillName: "React",
-    whatArea: "Framework",
   },
   {
     image: "/images/Firebase.svg",
-    skillName: "Firebase",
-    whatArea: "Database",
   },
   {
     image: "/images/figma.png",
-    skillName: "Figma",
-    whatArea: "Design",
   },
   {
     image: "/images/GitHub.svg",
-    skillName: "Github",
-    whatArea: "VCS",
   },
   {
     image: "/images/sass.png",
-    skillName: "SASS",
-    whatArea: "Extenssion for css",
   },
   {
     image: "/images/tailwind.png",
-    skillName: "Tailwind",
-    whatArea: "CSS framework",
   },
   {
     image: "/images/redux.svg",
-    skillName: "Redux",
-    whatArea: "State management tool",
   },
   {
     image: "/images/typescript.png",
-    skillName: "Typescript",
-    whatArea: "JS with superpowers",
   },
   {
     image: "/images/java-script.png",
-    skillName: "Javascript",
-    whatArea: "Coding language",
   },
   {
     image: "/images/html.png",
-    skillName: "HTML",
-    whatArea: "Structure",
   },
   {
     image: "/images/css-3.png",
-    skillName: "CSS",
-    whatArea: "Design",
   },
   {
     image: "/images/bootstrap.svg",
-    skillName: "Bootstrap",
-    whatArea: "CSS library",
   },
   {
     image: "/images/Express.svg",
-    skillName: "Express JS",
-    whatArea: "Node JS framework",
   },
   {
     image: "/images/Node.js.svg",
-    skillName: "Node JS",
-    whatArea: "Back-end language",
   },
   {
     image: "/images/MongoDB.svg",
-    skillName: "Mongo DB",
-    whatArea: "Database",
   },
   {
     image: "/images/Swagger.svg",
-    skillName: "Swagger",
-    whatArea: "Documentation",
+  },
+  {
+    image: "/images/postgresql.svg",
   },
 ];
 
